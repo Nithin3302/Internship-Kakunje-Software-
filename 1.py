6a8d799b6a309e43d3a133f420d8d691baa0ae1e
Nithin R Poojary <nithinrpoojary3302@gmail.com>
2026-02-02 20:10:31 +0530

    Added internship assignment tasks

---
+++ 1.py
@@ -0,0 +1,31 @@
+# { PYTHON BASICS}
+print('Hello World')
+
+
+# INDENTATION
+if 10>9 :
+    print("Hello")
+
+#This is a comment 
+""" 
+this is multiline
+"""
+
+#{ Continuation }
+# Implicit Continuation
+l = [1,2,3,4,5,6]
+print(l)
+
+s = [1,2,
+     3,4,
+     5,6]
+print(s)
+
+#Explicit Continuation
+t = "Python is easy to learn"\
+    "Python is easy"
+print(t)
+
+x = input('Enter num 1')
+y = input('Enter num 1')
+print(x+y)
---
+++ T1.py
@@ -0,0 +1,63 @@
+# ---------------------------------------------------------------------------
+name = "Nithin"
+age = 21
+height = 170
+
+print(name,age,height)
+
+# ---------------------------------------------------------------------------
+
+a, b, c = 5, 10, 15
+print("Sum:", a + b + c)
+
+# ---------------------------------------------------------------------------
+
+total = (
+    10 +
+    20 +
+    30 +
+    40
+)
+
+print("Total :", total)
+# ---------------------------------------------------------------------------
+
+message = (
+    "Python is  "
+    "an easy to "
+    "learn programming language"
+)
+print(message)
+# ---------------------------------------------------------------------------
+
+numbers = [
+    1, 2, 3, 4,
+    5, 6, 7, 8
+]
+
+print(numbers)
+# ---------------------------------------------------------------------------
+
+name = input("Enter your name: ")
+print("Welcome,", name)
+# ---------------------------------------------------------------------------
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum =", num1 + num2)
+print("Difference =", num1 - num2)
+print("Product =", num1 * num2)
+# ---------------------------------------------------------------------------
+
+x = input("Enter first value: ")
+y = input("Enter second value: ")
+
+print("Value of x:", x, "Type:", type(x))
+print("Value of y:", y, "Type:", type(y))
+
+x = int(x)
+y = int(y)
+
+print("Sum after conversion =", x + y)
+# ---------------------------------------------------------------------------
